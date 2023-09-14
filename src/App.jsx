76c0f18d9006201d02dd46f6292b8492df4e2ab2
Mehedihasan444 <mehedihasan67705251@gmail.com>
2023-09-14 19:46:42 +0600

    project setup with vite + react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import PropTypes from 'prop-types';
+import './App.css'
+import Course from './components/courses/Course';
+import Selected_course from './components/selected_course/Selected_course';
+function App() {
+  
+
+  return (
+    <>
+      <Course></Course>
+      <Selected_course></Selected_course>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/selected_course/Selected_course.jsx
@@ -0,0 +1,11 @@
+
+
+const Selected_course = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Selected_course;
